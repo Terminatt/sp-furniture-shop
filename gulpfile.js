5e4bf214e403be2f6be8b8fdc033168ac45ce6ab
Ernest Rudnicki <terminat12@gmail.com>
2019-11-25 18:35:30 +0100

    gulp configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,31 @@
 var gulp = require('gulp');
+var browserSync = require("browser-sync");
+var sass = require('gulp-sass');
+
+function transpileSass() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    )
+}
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+function watchFiles() {
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+  gulp.watch("app/sass/**/*.scss", transpileSass);
+  gulp.watch("app/**/*.html", reload);
+}
+
+exports.serve = gulp.series(transpileSass, watchFiles);
